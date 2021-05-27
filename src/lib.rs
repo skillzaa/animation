136a974e2217c7cbd3cf531c0081562d29d7a925
Bilal Tariq <skillzaa.com@gmail.com>
2021-05-27 19:56:46 +0500

    ver01

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 
 mod calc;
 pub use calc::{percent_to_value,percentage};
-use std::{io::{Error,ErrorKind}, ops::Div, u128};
+
 #[derive(Debug)]
 pub struct Animation {
     from_time:u128,
@@ -16,13 +16,13 @@ pub struct Animation {
 //==========================================
 
 impl Animation{
-    pub fn new(from_time:u128,to_time:u128,from:u128,to:u128,attr_to_animate:&str)->Result<Animation,Error>{
+    pub fn new(from_time:u128,to_time:u128,from:u128,to:u128,attr_to_animate:&str)->Option<Animation>{
     assert!(from_time < to_time,"From time can not be bigger than To time");   
     assert!(from < 5000,"From value is too large");
     assert!(to < 5000,"To value is too large");
         let from_time_millis:u128 = from_time * 1000;
         let to_time_millis:u128 = to_time * 1000;
-            Ok(Animation {
+            Some(Animation {
                 from_time : from_time_millis,
                 to_time : to_time_millis,
                 from,
@@ -50,13 +50,11 @@ impl Animation{
         let time_perc_lapsed = percent_to_value(self.animation_duration as f64, time_lapsed as f64);
         match time_perc_lapsed {
             Some(x)=>{  
-                //Some(x as u128)
-                let per = percentage(self.animation_distance as f64,x);
+                let per:Option<u128> = percentage(self.animation_distance as f64,x);
                     match per {
                         Some(y)=> {
                             if self.is_reverse == true {
-                                let f = y as u128 - self.to;
-                                return Some(f)
+                               return Some(self.from - y)
                             }else {
                                 let f = y as u128 + self.from;
                                 return Some(f)
@@ -66,25 +64,31 @@ impl Animation{
                     }
             },
             None=> return None,
-        }
-       
+        }       
     }
-    
+   
     }//end of impl block
 fn is_reverse(from:u128,to:u128)->bool{
     if from < to {
-        false
+        return false
     }else {
-        true
+        return true
     }
 }
 fn animation_duration(to_time:u128,from_time:u128)->u128{
-    let ret:u128 = to_time - from_time;
-    ret
+    if from_time > to_time {
+        return from_time - to_time
+    }else {
+        return to_time - from_time
+    }
 }
 fn animation_distance (from:u128,to:u128)->u128{
-    let c = to - from as u128;
-        c
+    if from > to {
+        return from - to
+    }else {
+        return to - from
+    }
 }
 
-///////////////////////////////===========tests
+//----public API
+
---
+++ tests/auto.rs
@@ -0,0 +1,103 @@
+use bilzaa2danimation::Animation;
+//use #[should_panic] with the test --if to check errors 
+fn test_a(a:Animation,time_ms:u128,answer:u128){
+    match a.animate(time_ms) {
+        Some(x)=> assert_eq!(x as u128,answer),
+        None=> panic!("Panic"),
+    }
+}
+fn get_a(from_time:u128,to_time:u128,from:u128,to:u128)->Animation{
+    let a = Animation::new(from_time, to_time, from, to, "width");
+    match a {
+        Some(b)=>{return b},
+        None=>panic!("Animation creation error"),
+}
+}
+//////////////////////////////////
+// #[cfg(test)]
+#[test]
+fn first(){
+  let a:Animation = get_a(0,10,0,100);  
+ test_a(a, 10000, 100);
+}
+#[test]
+fn mm(){
+  let a:Animation = get_a(0,10,0,100);  
+ test_a(a, 5000, 50);
+}
+#[test]
+#[should_panic] // time is not right it shd be 15000
+fn mmm(){
+  let a:Animation = get_a(10,20,0,100);  
+ test_a(a, 15, 50);
+}
+#[test]
+#[should_panic]
+fn auto_second(){
+  let a:Animation = get_a(0,10,0,100);  
+ test_a(a, 0, 0);
+}
+#[test]
+fn aa(){
+  let a:Animation = get_a(10,20,0,100);  
+ test_a(a, 15000, 50);
+}
+#[test]
+fn aaa(){
+  let a:Animation = get_a(10,20,100,200);  
+ test_a(a, 15000, 150);
+}
+#[test]
+fn reverse_one(){
+  let a:Animation = get_a(10,20,200,100);  
+ test_a(a, 15000, 150);
+}
+#[test]
+fn reverse_two(){
+  let a:Animation = get_a(10,20,200,100);  
+ test_a(a, 20000, 100);
+}
+#[test]
+#[should_panic] // time is not vlaid
+fn reverse_three(){ // its zero
+  let a:Animation = get_a(10,20,200,100);  
+ test_a(a, 0, 200);
+}
+#[test]
+#[should_panic]
+fn reverse_four(){ // its zero--start time--will ret none
+  let a:Animation = get_a(10,20,200,100);  
+ test_a(a, 10000, 200);
+}
+#[test]
+fn reverse_five(){//50%
+  let a:Animation = get_a(10,50,200,100);  
+ test_a(a, 30000, 150);
+}
+#[test]
+fn reverse_six(){//its 175 not 125 its reverse
+  let a:Animation = get_a(10,50,200,100);  
+ test_a(a, 20000, 175);
+}
+#[test]
+fn normal(){//its 125 now since not reverse
+  let a:Animation = get_a(10,50,100,200);  
+ test_a(a, 20000, 125);
+}
+#[test]
+fn normal_two(){//end
+  let a:Animation = get_a(10,50,100,200);  
+ test_a(a, 50000, 200);
+}
+#[test]
+#[should_panic]
+fn normal_three(){//end
+  let a:Animation = get_a(10,50,100,200);  
+ test_a(a, 10000, 100);
+}
+#[test]
+#[should_panic]
+fn time_wrong(){//end
+  let a:Animation = get_a(10,5,100,200);  
+ test_a(a, 10000, 100);
+}
