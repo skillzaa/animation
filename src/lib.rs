ab01e97c491fd81d82d0c900b3e61c574344e29a
Bilal Tariq <skillzaa.com@gmail.com>
2021-05-27 15:08:46 +0500

    why is this small problem so diff

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 
 mod calc;
-pub use calc::percent_to_value;
+pub use calc::{percent_to_value,percentage};
 use std::{io::{Error,ErrorKind}, ops::Div, u128};
 #[derive(Debug)]
 pub struct Animation {
@@ -11,13 +11,12 @@ pub struct Animation {
     is_reverse:bool, 
     animation_duration:u128,
     animation_distance:u128,
-    generator:String,
     attr_to_animate:String,
 }
 //==========================================
 
 impl Animation{
-    pub fn new(from_time:u128,to_time:u128,from:u128,to:u128,generator:&str,attr_to_animate:&str)->Result<Animation,Error>{
+    pub fn new(from_time:u128,to_time:u128,from:u128,to:u128,attr_to_animate:&str)->Result<Animation,Error>{
     assert!(from_time < to_time,"From time can not be bigger than To time");   
     assert!(from < 5000,"From value is too large");
     assert!(to < 5000,"To value is too large");
@@ -31,12 +30,44 @@ impl Animation{
                 is_reverse: is_reverse(from, to),
                 animation_duration : animation_duration(to_time_millis, from_time_millis), 
                 animation_distance:animation_distance(from, to),
-                generator:String::from(generator),
                 attr_to_animate:String::from(attr_to_animate),
             })
-    }       
-    pub fn animate(&self,time_ms:u128)->u128{      
-   88   
+    } 
+    fn is_time_valid(&self,time_ms:u128)->bool{
+        if time_ms > self.to_time || time_ms < self.from_time {
+            panic!("Time is not valid")
+           }else {
+               true
+           }       
+    }
+    fn time_lapsed(&self,time_ms:u128)->u128{
+        //--time is valid check is seperate.       
+        time_ms - self.from_time
+    }      
+    pub fn animate(&self,time_ms:u128)->Option<u128>{     
+        self.is_time_valid(time_ms);
+        let time_lapsed = self.time_lapsed(time_ms);
+        let time_perc_lapsed = percent_to_value(self.animation_duration as f64, time_lapsed as f64);
+        match time_perc_lapsed {
+            Some(x)=>{  
+                //Some(x as u128)
+                let per = percentage(self.animation_distance as f64,x);
+                    match per {
+                        Some(y)=> {
+                            if self.is_reverse == true {
+                                let f = y as u128 - self.to;
+                                return Some(f)
+                            }else {
+                                let f = y as u128 + self.from;
+                                return Some(f)
+                            }
+                        },
+                        None=> return None,   
+                    }
+            },
+            None=> return None,
+        }
+       
     }
     
     }//end of impl block
@@ -57,22 +88,3 @@ fn animation_distance (from:u128,to:u128)->u128{
 }
 
 ///////////////////////////////===========tests
-#[cfg(test)]
-#[test]
-fn basic(){
-    let a = Animation::new(0, 10, 0, 100, "generator", "width");
-    // println!("{:?}",a);
-    // match a {
-    //     Ok(b)=>{    
-    //         // assert!(b.animate(0),0);
-    //         // println!("at 0 ms x ={:?}",b.animate(1));
-    //         // println!("at 1000 ms x ={:?}",b.animate(1000));
-    //         println!("at 2500 ms x ={:?}",b.animate(3000));
-    //         println!("at 5000 ms x ={:?}",b.animate(5000));
-    //         assert!(true);
-    
-    //     },
-    //     Err(e)=>(),
-    // }
-      
-}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
-mod calc;
-use calc::percent_to_value;
+
+use bilzaa2danimation::Animation;
 fn main(){
-//   let a = percent_to_value(0,0);
-  percent_to_value(0_f64,10_f64);  
+    let a = Animation::new(0, 10, 0, 100, "width");
+    println!("{:?}",a);
+    match a {
+        Ok(b)=>{    
+           // println!("at 0 ms x ={:?}",b.animate(0));
+           
+            println!("at 1000 ms x ={:?}",b.animate(1000));
+            println!("at 5,000 ms x ={:?}",b.animate(5000));
+            println!("at 10,000 ms x ={:?}",b.animate(10000));
+    
+        },
+        Err(e)=>(),
+    }
+    
 }
